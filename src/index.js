e593ffcab6468503c9b63c081bb8acf5e5f6eae9
Radosław Kłos <admin@glxy.pl>
2018-06-16 19:48:42 +0200

    Better code quality

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 const path = require('path')
 
-/**
- * Returns suffix from shortcut
- * @param ref - String from import declaration
- * @param opts - Shortcut options
- * @returns {string}
- */
-function getSuffix (ref, opts) {
+function getSuffix (reference, options) {
   let suffix = null
-  for (let opt of opts) {
-    if (ref[0] === opt.pathPrefix) {
+  const refFirstChar = reference[0]
+
+  for (let opt of options) {
+    if (refFirstChar === opt.pathPrefix) {
       suffix = opt.pathSuffix
       break
     }
@@ -18,28 +14,21 @@ function getSuffix (ref, opts) {
   return suffix
 }
 
-/**
- * Returns relative path to shortcutted module path
- * @param ref - String from import declaration
- * @param opts - Shortcut options
- * @param sourceFile - Path to source file
- * @returns {string}
- */
-function getRelativePath (ref, opts, sourceFile) {
-  const cwd              = process.cwd()
-  const suffix           = getSuffix(ref, opts)
-  const requiredNoPrefix = ref.slice(1)
+function getRelativePath (reference, options, sourceFilePath) {
+  const suffix = getSuffix(reference, options)
+  if (!suffix) return reference
 
-  if (!suffix) return ref
-
-  const absolutePath = path.resolve(`${suffix}/${requiredNoPrefix}`)
-  let sourcePath     = sourceFile.substring(0, sourceFile.lastIndexOf('/'))
-  if (sourcePath.indexOf('/') === 0 || sourcePath.indexOf(':/') === 1 || sourcePath.indexOf(':\\') === 1) {
-    sourcePath = sourcePath.substring(cwd.length + 1)
+  sourceFilePath = sourceFilePath.substring(0, sourceFilePath.lastIndexOf('/'))
+  if (sourceFilePath.indexOf('/') === 0 || sourceFilePath.indexOf(':/') === 1 || sourceFilePath.indexOf(':\\') === 1) {
+    sourceFilePath = sourceFilePath.substring(process.cwd().length + 1)
   }
-  sourcePath = path.resolve(sourcePath)
 
-  let relativePath = path.relative(sourcePath, absolutePath)
+  sourceFilePath = path.resolve(sourceFilePath)
+  reference = reference.slice(1)
+
+  const absolutePath = path.resolve(`${suffix}/${reference}`)
+  let relativePath = path.relative(sourceFilePath, absolutePath)
+
   if (relativePath.indexOf('../') !== 0) {
     relativePath = './' + relativePath
   }
@@ -47,28 +36,21 @@ function getRelativePath (ref, opts, sourceFile) {
   return relativePath
 }
 
-/**
- * Babylon AST nodes
- * @param babel
- * @returns {object}
- */
-function shortcutImport (babel) {
+function ASTNodesDeclarations (babel) {
   return {
     visitor: {
       /**
        * An import declaration, e.g.
        *  import foo from "mod"
-       * @param path
-       * @param state
        */
       ImportDeclaration (path, state) {
         if (!babel.types.isLiteral(path.node.source)) return
 
-        const ref        = path.node.source.value
-        const opts       = state.opts
-        const sourceFile = state.file.opts.filename
+        const reference = path.node.source.value
+        const options = state.opts
+        const sourceFilePath = state.file.opts.filename
 
-        path.node.source.value = getRelativePath(ref, opts, sourceFile)
+        path.node.source.value = getRelativePath(reference, options, sourceFilePath)
       },
 
       /**
@@ -77,55 +59,49 @@ function shortcutImport (babel) {
        *  export {foo} from "mod"
        *  export var foo = 1
        *  export * as foo from "bar"
-       * @param path
-       * @param state
        */
       ExportNamedDeclaration (path, state) {
         if (path.node.source) return
 
-        const ref        = path.node.source.value
-        const opts       = state.opts
-        const sourceFile = state.file.opts.filename
+        const reference = path.node.source.value
+        const options = state.opts
+        const sourceFilePath = state.file.opts.filename
 
-        path.node.source.value = getRelativePath(ref, opts, sourceFile)
+        path.node.source.value = getRelativePath(reference, options, sourceFilePath)
       },
 
       /**
        * An export batch declaration, e.g.
        *  export * from "mod"
-       * @param path
-       * @param state
        */
       ExportAllDeclaration (path, state) {
         if (path.node.source) return
 
-        const ref        = path.node.source.value
-        const opts       = state.opts
-        const sourceFile = state.file.opts.filename
+        const reference = path.node.source.value
+        const options = state.opts
+        const sourceFilePath = state.file.opts.filename
 
-        path.node.source.value = getRelativePath(ref, opts, sourceFile)
+        path.node.source.value = getRelativePath(reference, options, sourceFilePath)
       },
 
-      /**
-       * A function or method call expression
-       * @param path
-       * @param state
-       */
       CallExpression (path, state) {
-        if (path.node.callee.name !== 'require') return
-        if (!path.node.arguments.length) return
-        if (!babel.types.isStringLiteral(path.node.arguments[0])) return
-        if (!path.node.arguments[0].value) return
+        const calleeName = path.node.callee.name
+        const args = path.node.arguments
+        const firstArg = args[0]
 
-        const arg        = path.node.arguments[0]
-        const ref        = arg.value
-        const opts       = state.opts
-        const sourceFile = state.file.opts.filename
+        if (calleeName !== 'require') return
+        if (!args.length) return
+        if (!babel.types.isStringLiteral(firstArg)) return
+        if (!firstArg.value) return
 
-        arg.value = getRelativePath(ref, opts, sourceFile)
+        const reference = firstArg.value
+        const options = state.opts
+        const sourceFilePath = state.file.opts.filename
+
+        firstArg.value = getRelativePath(reference, options, sourceFilePath)
       }
     }
   }
 }
 
-module.exports = shortcutImport
+module.exports = ASTNodesDeclarations
